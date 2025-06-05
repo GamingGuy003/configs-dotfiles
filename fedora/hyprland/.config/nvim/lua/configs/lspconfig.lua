b406d30bac05f7bc92c2831bbf95bfddb7398e45
GamingGuy003 <bjornfelixh@gmail.com>
2025-06-05 14:38:28 +0200

    Updates

--- fedora/hyprland/.config/nvim/lua/configs/lspconfig.lua
+++ fedora/hyprland/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "bashls", "marksman", "rust_analyzer", "slint_lsp" }
+local servers = { "html", "cssls", "clangd", "bashls", "marksman", "rust_analyzer", "slint_lsp", "autotools_ls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
